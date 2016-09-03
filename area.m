ebec0744f4625b7661bf55bab5a626e6f2c4f38c
Iaslan Nascimento <iaslan.nascimento@hotmail.com>
2016-09-02 20:47:01 -0700

    area de um objeto

---
+++ area.m
@@ -0,0 +1,15 @@
+close all
+clear all
+
+im = imread('')
+
+
+    area = 0;
+
+    for i=1:size(im,1)
+        for j=1:size(im,2)
+            if (im(i,j)==255)
+                area = area + 1;
+            end
+        end
+    end
